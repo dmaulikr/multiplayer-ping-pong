fa949be9401e2ad8e3e20024428984a2d03e2614
Sathvik777 <katam.sathvik@gmail.com>
2016-11-06 10:10:27 +0100

    Initial layout

---
+++ ping-pong/GameScene.swift
@@ -0,0 +1,40 @@
+//
+//  GameScene.swift
+//  ping-pong
+//
+//  Created by Sathvik Katam on 11/5/16.
+//  Copyright © 2016 Sathvik Katam. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+   
+    var Enemy = SKSpriteNode()
+    var Player = SKSpriteNode()
+    var Target = SKSpriteNode()
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+        Target = self.childNode(withName: "Target") as! SKSpriteNode
+        Player = self.childNode(withName: "Player") as! SKSpriteNode
+        Enemy = self.childNode(withName: "Enemy") as! SKSpriteNode
+        Target.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 0
+        self.physicsBody = border
+        
+        
+           }
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
